d3bd62ff0876a89fa097301d8210938ef2d1244a
diaHelicoprion00 <reedishigamiwayne00@gmail.com>
2024-07-25 14:25:03 -0400

    style: formatted with black

--- etl/models/anti_recommendation.py
+++ etl/models/anti_recommendation.py
@@ -9,10 +9,11 @@ class AntiRecommendation(NamedTuple):
     """
     A NamedTuple that contains data on an anti-recommendation.
 
-    `key` is the name of a Record that was converted into a Document. 
+    `key` is the name of a Record that was converted into a Document.
     `document` is the Document representation of a Record that was retrieved from a VectorStore.
     `similarity_score` is the similarity score of the Document that was retrieved from a VectorStore.
     """
+
     key: RecordKey
     document: Document
     similarity_score: float
